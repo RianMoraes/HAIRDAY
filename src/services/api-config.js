a7bffdebf3b15e7909ad1c71474f7cdcc214cc75
RianMoraes <rian.moraes2702@gmail.com>
2026-06-22 16:05:59 -0300

    criando o api-config

---
+++ src/services/api-config.js
@@ -0,0 +1,5 @@
+export const apiConfig = { 
+    baseURL: "http://localhost:8081",
+}
+// Example usage: referencia e recurso flexivel para acessar diferentes endpoints da API
+// `${apiConfig.baseURL}/schedule`
