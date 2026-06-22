08ac110801fcb8edb162b84e539d6dabe567622b
RianMoraes <rian.moraes2702@gmail.com>
2026-06-22 14:37:14 -0300

    renderizando os horarios

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
 import { openingHours } from "../../utils/opening-hours.js";
 
+const hours = document.getElementById('hours');
+
 export function hoursLoad({ date }) {
     const opening = openingHours.map(
         (hour) => {
@@ -9,11 +11,24 @@ export function hoursLoad({ date }) {
 
             //adiciona hora no date e verifica se está no passado
             const isHourPast = dayjs(date).add(scheduleHour, 'hour').isAfter(dayjs());
-            return{
-                hour, 
+            return {
+                hour,
                 available: isHourPast
             }
         }
     )
-    console.log(opening);
+
+    //renderiza os horarios
+    opening.forEach(({ hour, available }) => {
+        const li = document.createElement('li');
+
+        li.classList.add('hour')
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour;
+        hours.append(li);
+
+    })
+
+
 }
