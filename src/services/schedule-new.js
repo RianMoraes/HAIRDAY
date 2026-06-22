e8a519577eda3dddb72cdf54d0db4929ef23f9fd
RianMoraes <rian.moraes2702@gmail.com>
2026-06-22 16:11:13 -0300

    criando o serviço de agendamento

---
+++ src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import { apiConfig } from "./api-config.js";
+
+export async function scheduleNew({ id, name, when }) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedule`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json" //indica que o corpo da requisição é um JSON, para o backend interpretar corretamente
+            },
+            body: JSON.stringify({ id, name, when })//serializar, passar na requisição como texto
+        })
+
+        alert('Agendamento realizado com sucesso!');
+    } catch (error) {
+        console.error(error);
+        alert('Não foi possível criar o agendamento, tente novamente mais tarde');
+    }
+}
