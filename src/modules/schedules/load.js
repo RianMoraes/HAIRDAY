6b501f73417fb111c08568c800d4f448dabb3a5f
RianMoraes <rian.moraes2702@gmail.com>
2026-06-23 08:14:16 -0300

    renderizando os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,6 +1,6 @@
 import { scheduleFetchByDay } from '../../services/schedule-fetch-by-day.js'
 import { hoursLoad } from '../form/hours-load.js';
-
+import { schedulesShow } from '../schedules/show.js';
 //seleciona o input de data
 const selectedDate = document.getElementById('date');
 
@@ -10,8 +10,10 @@ export async function schedulesDay() {
 
     //busca na API os agendamentos
     const dailySchedules = await scheduleFetchByDay({ date });
-    console.log(dailySchedules);
-    
+
+    //exibe os agendamentos
+    schedulesShow({ dailySchedules });
+
     //renderiza as horas disponiveis
     hoursLoad({ date });
 
