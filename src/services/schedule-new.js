05e24edce962028312c238786fdf3e98109e1c3a
RianMoraes <rian.moraes2702@gmail.com>
2026-06-23 07:49:05 -0300

    serviço para buscar agendamentos do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({ id, name, when }) {
     try {
+        //faz requisição para enviar os dados do agendamento
         await fetch(`${apiConfig.baseURL}/schedules`, {
             method: 'POST',
             headers: {
@@ -9,7 +10,7 @@ export async function scheduleNew({ id, name, when }) {
             },
             body: JSON.stringify({ id, name, when })//serializar, passar na requisição como texto
         })
-
+        //se não houver erro, exibe mensagem de sucesso
         alert('Agendamento realizado com sucesso!');
     } catch (error) {
         console.error(error);
